467b6280b6abc3982160a0800b4c0ef3b8aa9894
Matblube <119967325+Matblube@users.noreply.github.com>
2025-10-01 22:25:02 -0400

    Add files via upload

---
+++ nose_files/script.js
@@ -0,0 +1,7 @@
+const sea = lottie.loadAnimation({
+    container: document.getElementById('sea'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://static.igem.wiki/teams/5993/animations/seawave.json' // the path to the animation json
+  });
